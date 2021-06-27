2bc0ad9be75217ee4fcd2d5b073e12dd4345284b
srikrishnas <srikrisna1234@gmail.com>
2021-06-27 16:32:17 +0530

    space

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -11,7 +11,7 @@ module.exports = (req,res,next) =>{
     const token = authorization.replace("Bearer ","")
     jwt.verify(token, process.env.JWT_SECRET,(err,payload)=>{
         if(err){
-            return res.status(401).json({error:"you must be logged in"})
+            return res.status(401).json({error:"you  must be logged in"})
         }
         const {_id} = payload
         User.findById(_id).then(userdata=>{
